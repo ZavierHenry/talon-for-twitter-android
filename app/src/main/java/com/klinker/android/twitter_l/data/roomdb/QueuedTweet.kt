ba9b72a533ed970acba6775249cec2ca8f080f49
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-04 00:13:22 -0500

    Create queued tweet entity

---
+++ app/src/main/java/com/klinker/android/twitter_l/data/roomdb/QueuedTweet.kt
@@ -0,0 +1,25 @@
+package com.klinker.android.twitter_l.data.roomdb
+
+import androidx.room.*
+
+@Entity(tableName = "queued_tweets")
+data class QueuedTweet(
+        val text: String,
+        val account: Int,
+        @PrimaryKey var id: Long? = null
+)
+
+@Dao
+interface QueuedTweetDao {
+
+    @Insert
+    fun insertQueuedTweet(tweet: QueuedTweet) : Long
+
+    @Delete
+    fun deleteQueuedTweet(tweet: QueuedTweet)
+
+
+    @Query("SELECT * FROM queued_tweets WHERE account = :account")
+    fun getQueuedTweets(account: Int) : List<QueuedTweet>
+
+}

54203b6b4d59975cac0bcb51bdf35ef187826948
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-06 11:19:30 -0500

    Updated tweet and user data classes

--- app/src/main/java/com/klinker/android/twitter_l/data/roomdb/Tweet.kt
+++ app/src/main/java/com/klinker/android/twitter_l/data/roomdb/Tweet.kt
@@ -1,15 +1,52 @@
 package com.klinker.android.twitter_l.data.roomdb
 
+import androidx.core.text.HtmlCompat
+import androidx.room.ColumnInfo
 import androidx.room.Embedded
+import com.klinker.android.twitter_l.utils.TweetLinkUtils
+import twitter4j.Status
 
 data class Tweet(
-        @Embedded val author: User,
+        @ColumnInfo(name = "tweet_id") val tweetId: Long,
+        @Embedded(prefix = "user_") val author: User,
         val text: String,
         val time: Long,
-        val likes: Int,
-        val retweets: Int,
         val hashtags: List<String>,
+        val mentions: List<String>,
+        val images: List<String>,
+        val urls: List<String>,
         val account: Int,
-        val liked: Boolean?,
-        val retweeted: Boolean?
-)
+        val likes: Int? = null,
+        val retweets: Int? = null,
+        val liked: Boolean? = null,
+        val retweeted: Boolean? = null,
+        val source: String? = null,
+        @Embedded(prefix = "retweeter_") val retweeter: User? = null
+) {
+
+    constructor(status: Status, account: Int) : this(
+            status,
+            if (status.isRetweet) status.retweetedStatus else status,
+            TweetLinkUtils.getLinksInStatus(if (status.isRetweet) status.retweetedStatus else status),
+            account
+    )
+
+    private constructor(status: Status, originalStatus: Status, linksInStatus: Array<String>, account: Int) : this(
+            status.id,
+            User(originalStatus.user, account),
+            linksInStatus[0],
+            status.createdAt.time,
+            linksInStatus[3].split(" "),
+            linksInStatus[4].split(" "),
+            linksInStatus[1].split(" "),
+            linksInStatus[2].split(" "),
+            account,
+            originalStatus.favoriteCount,
+            originalStatus.retweetCount,
+            originalStatus.isFavorited,
+            originalStatus.isRetweetedByMe,
+            HtmlCompat.fromHtml(originalStatus.source, HtmlCompat.FROM_HTML_MODE_LEGACY).toString(),
+            if (status.id != originalStatus.id) User(status.user, account) else null
+    )
+
+}
--- app/src/main/java/com/klinker/android/twitter_l/data/roomdb/User.kt
+++ app/src/main/java/com/klinker/android/twitter_l/data/roomdb/User.kt
@@ -1,11 +1,20 @@
 package com.klinker.android.twitter_l.data.roomdb
 
 import androidx.room.ColumnInfo
+import twitter4j.User as TwitterUser
 
 data class User(
         @ColumnInfo(name = "screen_name") val screenName: String,
-        val name: String,
-        @ColumnInfo(name = "profile_pic") val profilePic: String,
         val account: Int,
+        val name: String? = null,
+        @ColumnInfo(name = "profile_pic") val profilePic: String? = null,
         @ColumnInfo(name = "user_id") val userId: Long? = null
-)
+) {
+    constructor(user: TwitterUser, account: Int) : this(
+            user.screenName,
+            account,
+            user.name,
+            user.originalProfileImageURLHttps ?: user.originalProfileImageURL,
+            user.id
+    )
+}

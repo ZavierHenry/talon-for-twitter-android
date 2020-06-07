f84f47ed436089b9c6fef6f9006aca3c54a310cd
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-07 00:07:53 -0500

    Create user tweet entity and dao

---
+++ app/src/main/java/com/klinker/android/twitter_l/data/roomdb/UserTweet.kt
@@ -0,0 +1,29 @@
+package com.klinker.android.twitter_l.data.roomdb
+
+import androidx.room.*
+import twitter4j.Status
+
+
+@Entity(tableName = "user_tweets")
+data class UserTweet(
+        @Embedded val tweet: Tweet,
+        val account: Int,
+        @PrimaryKey val id: Long? = null
+) {
+    constructor(status: Status, account: Int) : this(Tweet(status), account)
+}
+
+
+@Dao
+interface UserTweetDao {
+
+    @Insert
+    fun insertUserTweet(userTweet: UserTweet) : Long?
+
+    @Delete
+    fun deleteUserTweet(userTweet: UserTweet)
+
+    @Query("SELECT * FROM user_tweets WHERE account = :account ORDER BY tweet_id ASC LIMIT :pageSize OFFSET ((:page - 1) * :pageSize)")
+    fun getUserTweets(account: Int, page: Int = 1, pageSize: Int = 250)
+
+}

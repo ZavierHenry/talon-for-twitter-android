f71ef483d3e088995fbf86cab0a1955bece919d0
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-07 13:08:55 -0500

    Create list tweet entity

---
+++ app/src/main/java/com/klinker/android/twitter_l/data/roomdb/ListTweet.kt
@@ -0,0 +1,29 @@
+package com.klinker.android.twitter_l.data.roomdb
+
+import androidx.room.*
+import twitter4j.Status
+
+
+@Entity(tableName = "list_tweets")
+data class ListTweet(
+        @Embedded val tweet: Tweet,
+        @ColumnInfo(name = "list_id") val listId: Long,
+        val account: Int,
+        @PrimaryKey val id: Long? = null
+) {
+    constructor(status: Status, listId: Long, account: Int) : this(Tweet(status), listId, account)
+}
+
+
+@Dao
+interface ListTweetDao {
+
+    @Insert
+    fun insertListTweet(listTweet: ListTweet) : Long?
+
+    @Delete
+    fun deleteListTweet(listTweet: ListTweet)
+
+    @Query("SELECT * FROM list_tweets WHERE account = :account AND list_id = :listId ORDER BY tweet_id ASC LIMIT :pageSize OFFSET ((:page - 1) * :pageSize)")
+    fun getListTweets(account: Int, listId: Long, page: Int = 1, pageSize: Int = 200)
+}

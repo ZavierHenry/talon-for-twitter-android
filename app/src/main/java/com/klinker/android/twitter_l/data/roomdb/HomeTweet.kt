cf1563496b200a3b62226fd75300c1dacd9e5f01
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-07 13:26:39 -0500

    Create home tweet entity and dao

---
+++ app/src/main/java/com/klinker/android/twitter_l/data/roomdb/HomeTweet.kt
@@ -0,0 +1,31 @@
+package com.klinker.android.twitter_l.data.roomdb
+
+import androidx.room.*
+import twitter4j.Status
+
+
+@Entity(tableName = "home_tweets")
+data class HomeTweet(
+        @Embedded val tweet: Tweet,
+        val account: Int,
+        @ColumnInfo(name = "is_unread") val isUnread: Boolean,
+        @PrimaryKey val id: Long? = null
+) {
+    constructor(status: Status, account: Int, isUnread: Boolean) : this(Tweet(status), account, isUnread)
+}
+
+
+@Dao
+interface HomeTweetDao {
+
+    @Insert
+    fun insertHomeTweet(homeTweet: HomeTweet) : Long?
+
+    @Delete
+    fun deleteHomeTweet(homeTweet: HomeTweet)
+
+    @Query("SELECT * FROM home_tweets WHERE account = :account ORDER BY tweet_id DESC LIMIT :pageSize OFFSET ((:page - 1) * :pageSize)")
+    fun getHomeTweets(account: Int, page: Int = 1, pageSize: Int = 200) : List<HomeTweet>
+
+
+}

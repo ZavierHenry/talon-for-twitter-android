65d003bafc75e5a6c36f0e011992534630bf36ee
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-09 21:03:13 -0500

    Added more follower dao tests

--- app/src/androidTest/java/com/klinker/android/twitter_l/RoomDatabaseFollowerTest.kt
+++ app/src/androidTest/java/com/klinker/android/twitter_l/RoomDatabaseFollowerTest.kt
@@ -9,7 +9,7 @@ import com.klinker.android.twitter_l.data.roomdb.Follower
 import com.klinker.android.twitter_l.data.roomdb.FollowerDao
 import com.klinker.android.twitter_l.data.roomdb.TalonDatabase
 import com.klinker.android.twitter_l.data.roomdb.User
-import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -21,7 +21,17 @@ import java.io.IOException
 class RoomDatabaseFollowerTest {
     private lateinit var followerDao : FollowerDao
     private lateinit var db : TalonDatabase
-
+    
+    private fun makeMockFollower(
+            screenName: String = "chrislhayes", 
+            name: String? = "Chris Hayes",
+            profilePic: String? = "",
+            userId: Long? = 1023123L,
+            account: Int = 1
+    ) : Follower {
+        return Follower(User(screenName, name, profilePic, userId), account)
+    }
+    
     @Before
     fun createTalonDatabase() {
         val context = ApplicationProvider.getApplicationContext<Context>()
@@ -34,21 +44,43 @@ class RoomDatabaseFollowerTest {
     fun closeTalonDatabase() {
         db.close()
     }
-
-
+    
     @Test
     @Throws(Exception::class)
     fun insertFollower() {
-        val follower = Follower(
-                User("chrislhayes", "Chris Hayes", "", 1023482L),
-                1
-        )
-
+        val follower = makeMockFollower(account = 1)
         val id = followerDao.insertFollower(follower)
         val followers = followerDao.getFollowers(1)
         assertThat(followers.size, equalTo(1))
         assertThat(followers[0].id, equalTo(id))
     }
 
+
+    @Test
+    @Throws(Exception::class)
+    fun deleteFollower() {
+        val follower = makeMockFollower(account = 1)
+        val id = followerDao.insertFollower(follower)
+        assertThat(id, notNullValue())
+        followerDao.deleteFollower(follower.copy(id = id))
+        assertThat(followerDao.getFollowers(1).size, equalTo(0))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getFollowersFilterAccount() {
+        val followers = List(10) {
+            makeMockFollower("chrislhayes${it}", "Chris Hayes ${it}", "image ${it}", it.toLong(), if (it > 5) 2 else 1)
+        }
+        val insertedFollowers = followers.mapNotNull {
+            follower -> followerDao.insertFollower(follower)?.let { id -> follower.copy(id = id) }
+        }
+        assertThat(insertedFollowers.size, equalTo(followers.size))
+        val databaseFollowers = followerDao.getFollowers(2)
+        assertThat(
+                databaseFollowers.map { it.id }.sortedBy { it },
+                contains(*insertedFollowers.filter { it.account == 2}.map { it.id }.sortedBy { it }.toTypedArray())
+        )
+    }
 
 }

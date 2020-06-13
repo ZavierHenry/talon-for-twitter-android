7022a6f8ce53fee134f539297571610df102d829
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-12 23:03:48 -0500

    Create draft test class

---
+++ app/src/androidTest/java/com/klinker/android/twitter_l/RoomDatabaseDraftTest.kt
@@ -0,0 +1,43 @@
+package com.klinker.android.twitter_l
+
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.klinker.android.twitter_l.data.roomdb.Draft
+import com.klinker.android.twitter_l.data.roomdb.DraftDao
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.notNullValue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class RoomDatabaseDraftTest {
+    private lateinit var draftDao: DraftDao
+
+    private fun makeMockDraft(account: Int, text: String = "") : Draft {
+        return Draft(text, account)
+    }
+
+    @get:Rule val database = TestDatabase()
+
+    @Before
+    fun getDraftDao() {
+        draftDao = database.database.draftDao()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testInsertDraft() {
+        val draft = makeMockDraft(1)
+        val id = draftDao.insertDraft(draft)
+        assertThat(id, notNullValue())
+        val drafts = draftDao.getDrafts(1)
+        assertThat(drafts.size, equalTo(1))
+        assertThat(drafts[0].id, equalTo(id))
+    }
+
+
+
+}

86b96b98bd5679fefa320358584ef3f903f3fb00
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-06-22 02:32:26 -0500

    Set up direct message trim database tests

--- app/src/androidTest/java/com/klinker/android/twitter_l/daotests/RoomDatabaseDirectMessageTest.kt
+++ app/src/androidTest/java/com/klinker/android/twitter_l/daotests/RoomDatabaseDirectMessageTest.kt
@@ -41,7 +41,41 @@ class RoomDatabaseDirectMessageTest {
         assertThat(database.size, equalTo(1))
 
         directMessageDao.delete(directMessage.directMessage.copy(id = id))
-        assertThat(database.size, equalTo(0))
+        assertThat("Direct message was not properly deleted from the database", database.size, equalTo(0))
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun testTrimDatabase_NoTrim() {
+        val directMessages = List(10) {
+            MockDirectMessage(1, twitterId = it.toLong() + 1)
+        }
+
+        directMessages.forEach { database.insertIntoDatabase(it) }
+        assertThat("Problem setting up initial entities in database", database.size, equalTo(directMessages.size))
+        directMessageDao.trimDatabase(1, 20)
+        assertThat("Number of entries in database changed", database.size, equalTo(directMessages.size))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testTrimDatabase_NoTrim_RespectAccount() {
+        val directMessages = List(10) {
+            MockDirectMessage(1, twitterId = it.toLong() + 1)
+        }
+
+        val secondAccountDirectMessage = MockDirectMessage(2, 300L)
+
+        directMessages.forEach { database.insertIntoDatabase(it) }
+        database.insertIntoDatabase(secondAccountDirectMessage)
+
+        assertThat("Problem setting up initial entries in database", database.size, equalTo(directMessages.size + 1))
+        directMessageDao.trimDatabase(1, directMessages.size)
+        assertThat("Trim database does not respect account of entity", database.size, equalTo(directMessages.size + 1))
+
+    }
+
+
+
 
 }

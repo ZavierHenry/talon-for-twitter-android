f39c9c5f330b701828b99c324bf24142b2f1c1ab
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2020-07-03 00:01:17 -0500

    Create user tweet transfer database test shell

--- app/src/androidTest/java/com/klinker/android/twitter_l/transfertests/UserTweetTransferDatabaseTest.kt
+++ app/src/androidTest/java/com/klinker/android/twitter_l/transfertests/UserTweetTransferDatabaseTest.kt
@@ -14,8 +14,26 @@ import org.junit.runner.RunWith
 class UserTweetTransferDatabaseTest {
 
     private val sourceColumns = listOf(
-            "${UserTweetsSQLiteHelper.COLUMN_ID} integer primary key"
-            //Implement rest of source column tweets
+            "${UserTweetsSQLiteHelper.COLUMN_ID} integer primary key",
+            "${UserTweetsSQLiteHelper.COLUMN_ACCOUNT} integer",
+            "${UserTweetsSQLiteHelper.COLUMN_TWEET_ID} integer",
+            "${UserTweetsSQLiteHelper.COLUMN_UNREAD} integer",
+            "${UserTweetsSQLiteHelper.COLUMN_ARTICLE} text",
+            "${UserTweetsSQLiteHelper.COLUMN_TEXT} text not null",
+            "${UserTweetsSQLiteHelper.COLUMN_NAME} text",
+            "${UserTweetsSQLiteHelper.COLUMN_PRO_PIC} text",
+            "${UserTweetsSQLiteHelper.COLUMN_SCREEN_NAME} text",
+            "${UserTweetsSQLiteHelper.COLUMN_TIME} integer",
+            "${UserTweetsSQLiteHelper.COLUMN_URL} text",
+            "${UserTweetsSQLiteHelper.COLUMN_PIC_URL} text",
+            "${UserTweetsSQLiteHelper.COLUMN_HASHTAGS} text",
+            "${UserTweetsSQLiteHelper.COLUMN_USERS} text",
+            "${UserTweetsSQLiteHelper.COLUMN_RETWEETER} text",
+            "${UserTweetsSQLiteHelper.COLUMN_USER_ID} integer",
+            "${UserTweetsSQLiteHelper.COLUMN_ANIMATED_GIF} text",
+            "${UserTweetsSQLiteHelper.COLUMN_EXTRA_TWO} text",
+            "${UserTweetsSQLiteHelper.COLUMN_EXTRA_THREE} text",
+            "${UserTweetsSQLiteHelper.COLUMN_MEDIA_LENGTH} integer default -1"
     )
 
     @get:Rule val database = with(ApplicationProvider.getApplicationContext<Context>()) {
